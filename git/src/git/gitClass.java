3ede0b1fe5844f4df2d8b62f660a96f23f3a8fd1
admin <admin@DESKTOP-I6HUGBR>
2024-12-05 13:56:02 +0900

    Change in master branch

--- git/src/git/gitClass.java
+++ git/src/git/gitClass.java
@@ -4,5 +4,6 @@ public class gitClass {
 	public void main() {
 		System.out.println("Hello Git");
 		System.out.println("Park sanghyuk");
+		System.out.println("XXX");
 	}
 }
--- git/src/git/gitClassTwo.java
+++ git/src/git/gitClassTwo.java
@@ -3,5 +3,6 @@ package git;
 public class gitClassTwo {
 	public void main() {
 		System.out.println("PPAP");
+		System.out.println("AOT");
 	}
 }
